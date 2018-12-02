85c2240b43965686a855174c2d8c0bfc5d64706c
hermespara <32836483+hermespara@users.noreply.github.com>
2018-12-02 13:47:51 +0100

    finishing POST and DELETE methods

--- flask_app.py
+++ flask_app.py
@@ -60,7 +60,7 @@ def view(id):
 def del_id(id):
     db = sqlite3.connect(DATABASE)
     c = db.cursor()
-    c.execute("DELETE FROM Genes WHERE Ensembl_Gene_ID ='%s'" % id)
+    c.execute("DELETE FROM Genes WHERE Ensembl_Gene_ID =?", [id])
     db.commit()
     return redirect(url_for("Genes"))
 
@@ -94,27 +94,48 @@ def new():
 
 #### API  = Application Programming Interface ####
 
-@app.route("/api/Genes/<id>", methods = ['GET'])
-def api_gene_id(id):
+def connect_to_db():
     db = sqlite3.connect(DATABASE)
+    return db
+
+@app.route("/api/Genes/<id>", methods = ['GET', 'DELETE'])
+def api_gene_id(id):
+    #db = sqlite3.connect(DATABASE)
+    db = connect_to_db()
     c = db.cursor()
-    c.execute("SELECT * FROM Genes WHERE Ensembl_Gene_ID =?", [id])
-    res = c.fetchone()
     colnames = []
     d = {}
-    if res:
-        for i in c.description:
-            colnames.append(i[0])
-        d = dict(zip(colnames, res))
-        transcript = c.execute("SELECT Ensembl_Transcript_ID, Transcript_Start, Transcript_End FROM Transcripts WHERE Ensembl_Gene_ID =?", [id])
-        for a in transcript:
-            d["transcripts"] = [dict(zip([c[0] for c in transcript.description], a))]
-        out = jsonify(d)
-    else:
-        d["error"] = "Ce gène n'existe pas"
-        out = jsonify(d)
-        out.status_code=404
-    db.close()
+    if request.method == 'GET':
+        c.execute("SELECT * FROM Genes WHERE Ensembl_Gene_ID =?", [id])
+        res = c.fetchone()
+        if res:
+            for i in c.description:
+                colnames.append(i[0])
+            d = dict(zip(colnames, res))
+            d["href"] = url_for('api_gene_id', id=d["Ensembl_Gene_ID"], _external = True)
+            transcript = c.execute("SELECT Ensembl_Transcript_ID, Transcript_Start, Transcript_End FROM Transcripts WHERE Ensembl_Gene_ID =?", [id])
+            for a in transcript:
+                d["transcripts"] = [dict(zip([c[0] for c in transcript.description], a))]
+            out = jsonify(d)
+        else:
+            d["error"] = "Ce gène n'existe pas"
+            out = jsonify(d)
+            out.status_code=404
+        connect_to_db().close()
+    if request.method == 'DELETE':
+        c.execute("SELECT * FROM Genes WHERE Ensembl_Gene_ID =?", [id])
+        res = c.fetchone()
+        if res:
+            c.execute("DELETE FROM Genes WHERE Ensembl_Gene_ID = ?", [id])
+            db.commit()
+            d["deleted"] = str(id) + " is deleted"
+            out = jsonify(d)
+            out.status_code = 200
+        else:
+            d["error"] = "Ce gène n'existe pas"
+            out = jsonify(d)
+            out.status_code=404
+
     return out
 
 @app.route("/api/Genes/", methods = ['GET'])
@@ -138,15 +159,16 @@ def api_genes():
         """,[offset])
 
     genes_list = []
-    gene_dict = {}
+    #gene_dict = {}
     for row in genes_api:
-        genes_dict = [dict(zip([c[0] for c in genes_api.description], row))]
+        genes_dict = dict(zip([c[0] for c in genes_api.description], row))
+        genes_dict["href"] = url_for("api_gene_id", id=row[0], _external = True)
         genes_list.append(genes_dict)
     new_gene_dict = {}
     new_gene_dict["items"] = genes_list
     new_gene_dict["first"] = offset + 1
-    new_gene_dict["last"] = len(genes_list)
-    new_gene_dict["next"] = url_for("api_genes",_external=True) + "?offset" + str(offset + 100)
+    new_gene_dict["last"] = len(genes_list) + offset
+    new_gene_dict["next"] = url_for("api_genes",_external=True) + "?offset=" + str(offset + 100)
     new_gene_dict["prev"] = url_for("api_genes",_external=True) + "?offset=" + str(offset)
     genes_out = jsonify(new_gene_dict)
     return genes_out
@@ -156,24 +178,22 @@ def validate_json(data_json):
 
     obligatory_fields=["Ensembl_Gene_ID","Chromosome_Name","Gene_Start","Gene_End"]
     optionnal_fields=["Band","Strand","Associated_Gene_Name"]
-    print(data_json.keys())
     # Check if data is a dict format.
     if isinstance(data_json,dict):
         # check if the fields of input data are instance of the datatable.
         for key in data_json.keys():
-            print(key)
             if key not in obligatory_fields + optionnal_fields:
                 msg = key + " is not an instance of this datatable. "
-                out = jsonify({"error": msg})
-                out.status_code = 403
-                return out
+                #out = jsonify({"error": msg})
+                status_code = 403
+                return msg, status_code
 
         for nedded_field in obligatory_fields:
             if nedded_field not in data_json.keys():
                     msg = nedded_field + " is nedded."
-                    out = jsonify({"error": msg})
-                    out.status_code = 403
-                    return out
+                    #out = jsonify({"error": msg})
+                    status_code = 403
+                    return msg, status_code
 
         for not_nedded_field in optionnal_fields:
             if not_nedded_field not in data_json.keys():
@@ -190,61 +210,120 @@ def validate_json(data_json):
 
         if not isinstance(ID, str):
             msg = "Ensembl_Gene_ID should be a string"
-            out = jsonify({"Type error": msg})
-            out.status_code = 403
-            return out
+            #out = jsonify({"Type error": msg})
+            status_code = 403
+            #return msg, status_code
         elif not isinstance(chr_name, str):
             msg = "Chromosome_Name should be a string"
-            out = jsonify({"Type error": msg})
-            out.status_code = 403
-            return out
+            #out = jsonify({"Type error": msg})
+            status_code = 403
+            #return msg, status_code
         elif not isinstance(band, str) and band != None:
             msg = "Band should be a string"
-            out = jsonify({"Type error": msg})
-            out.status_code = 403
-            return out
+            #out = jsonify({"Type error": msg})
+            status_code = 403
+            #return msg, status_code
 
         elif not isinstance(ass_gene_name, str) and ass_gene_name != None:
             msg = "Associated_Gene_Name should be a string"
-            out = jsonify({"Type error": msg})
-            out.status_code = 403
-            return out
+            #out = jsonify({"Type error": msg})
+            status_code = 403
+            #return msg, status_code
 
         elif not isinstance(gene_start, int):
             msg = "Gene_Start should be an integer"
-            out = jsonify({"Type error": msg})
-            out.status_code = 403
-            return out
+            #out = jsonify({"Type error": msg})
+            status_code = 403
+            #return out, status_code
         elif not isinstance(gene_end, int):
             msg = "Gene_End should be an integer"
-            out = jsonify({"Type error": msg})
-            out.status_code = 403
-            return out
+            #out = jsonify({"Type error": msg})
+            status_code = 403
+            #return out, status_code
         elif gene_start > gene_end:
             msg = "Gene_Start cannot be higher than Gene_End"
-            out = jsonify({"Type Error": msg})
-            out.status_code = 416
-            return out
+            #out = jsonify({"Type Error": msg})
+            status_code = 416
+            #return msg, status_code
         elif not isinstance(strand, int) and  strand != None and strand not in [-1,1]:
             msg = "Strand should be an integer : -1 for complementary strand and 1 for matrice brand"
-            out = jsonify({"Type Error": msg})
-            out.status_code = 403
-            return out
+            #out = jsonify({"Type Error": msg})
+            status_code = 403
+            #return msg, status_code
+
+        else:
+            #db = connect_to_db()
+            #db.execute("""
+            #  INSERT INTO Genes
+            #  (Ensembl_Gene_ID, Chromosome_Name, Band, Strand, Gene_Start,
+            #   Gene_End,Associated_Gene_Name)
+            #  VALUES (?,?,?,?,?,?,?)
+            #  """, [ID,chr_name,band,strand,gene_start,gene_end,ass_gene_name])
+            #c.execute("INSERT INTO Genes (Ensembl_Gene_ID, Chromosome_Name, Band, Strand, Gene_Start, Gene_End, Associated_Gene_Name, Transcript_Count) VALUES (?,?,?,?,?,?,?,?)",(gene_id,chr_name,band,strand,start,end,asso,count))
+            #db.commit()
+            #msg = url_for('api_gene_id', id=ID, _external = True)
+            #out = jsonify({"created": msg})
+            #out.status_code = 201
+            #return out
+            #msg = url_for('api_gene_id', id=i["Ensembl_Gene_ID"], _external = True)
+            #status_code
+            return True
+        return msg, status_code
 
 
-
-
-
-        return jsonify(data_json)
-
+def create_a_gene(data_dict):
+    db = connect_to_db()
+    db.execute("""INSERT INTO Genes (Ensembl_Gene_ID, Chromosome_Name, Band, Strand, Gene_Start, Gene_End,Associated_Gene_Name)
+        VALUES (?,?,?,?,?,?,?)""",
+        [data_dict["Ensembl_Gene_ID"], data_dict["Chromosome_Name"], data_dict["Band"], data_dict["Strand"], data_dict["Gene_Start"],data_dict["Gene_End"],data_dict["Associated_Gene_Name"]])
+    db.commit()
 
 
 @app.route("/api/Genes/", methods = ['POST'])
 def api_post_gene():
     json_post = request.get_json()
+    json_post = [json_post]
+    if not json_post:
+        json_post = [{
+          "Associated_Gene_Name": "TSPAN6",
+          "Band": "q22.1",
+          "Chromosome_Name": "X",
+          "Ensembl_Gene_ID": "ENSG00000000003",
+          "Gene_End": 99894988,
+          "Gene_Start": 99883667,
+          "Strand": -1
+        },
+        {
+          "Associated_Gene_Name": "TNMD",
+          "Band": "q22.1",
+          "Chromosome_Name": "X",
+          "Ensembl_Gene_ID": "ENSG00000000005",
+          "Gene_End": 99854882,
+          "Gene_Start": 99839799,
+          "Strand": -1
+        }]
     #print(json_post.keys())
     #validate_json(json_post)
-    return validate_json(json_post)#str(json_post)
+    url = []
+    for i in json_post:
+        if validate_json(i) == True:
+            create_a_gene(i)
+            msg = url_for('api_gene_id', id=i["Ensembl_Gene_ID"], _external = True)
+            url.append(msg)
+            out = jsonify({"url_created": msg})
+            #print(validate_json(i).status_code)
+            #return out
+            #return jsonify({"url": url})
+        else:
+            msg, status_code = validate_json(i)
+            #iop = jsonify(i)
+            #m = validate_json(i)
+            #mess = jsonify({"prob": m})
+            #return iop, m
+            out = jsonify({"error": msg + " pour le gene " + str(i)})
+            return out
+
+    return jsonify({"url": url}) #validate_json(json_post)#str(json_post)
 
 
 
